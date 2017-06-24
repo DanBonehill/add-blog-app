f3a7a46ba2169b5c3271dd5be4f6ba69106b84b1
Dan Bonehill <Danbonehill@me.com>
2017-06-24 07:39:34 +0000

    Hide edit and delete links from non-signed in and non-owner users

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.feature "Showing an Article" do 
   
   before do
-    @john = User.create(email: "John@example.com", password: "password")
-    login_as(@john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @steve = User.create(email: "steve@example.com", password: "password")
     @article = Article.create(title: "My First Article", body: "This is my first article!", user: @john)
   end
   
-  scenario "A user views an article" do
+  scenario "to non-signed in user hide the edit and delete button" do
     visit "/"
     
     click_link @article.title
@@ -16,5 +16,35 @@ RSpec.feature "Showing an Article" do
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "to non-owner hide the edit and delete button" do
+    login_as(@steve)
+    
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "to owner show the edit and delete button" do
+    login_as(@john)
+    
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 end
